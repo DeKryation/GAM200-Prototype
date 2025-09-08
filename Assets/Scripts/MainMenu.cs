723c5de2dce30a900b260eb798e169506ce2bc91
DeKryation <lucaschua01@gmail.com>
2025-09-08 19:43:16 +0800

    Start Button > Level 1 Scene
    Makeshift SceneManager Created in "MainMenu" Script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
 
+    }
+
+    public void GoToSceneLevelOne()              //This is attached to the button in unity
+    {
+        SceneManager.LoadScene("Lucas' Scene");  //RMB TO UPDATE (NAME) IF SCENE NAME CHANGES
     }
 
     public void QuitGame()
