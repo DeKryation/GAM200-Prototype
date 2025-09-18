278c53d0e5181c9b794c9b223174d89115370523
Crunchychipz <e.foo@digipen.edu>
2025-09-18 11:10:21 +0800

    Refined the PauseMenu
    PauseMenu can be seen in the MRTLevelScene. Fixed / Input the functions of 'resume' and 'main menu' button.

--- Assets/Scripts/Pause/PauseMenu.cs
+++ Assets/Scripts/Pause/PauseMenu.cs
@@ -1,27 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
     public GameObject PausePanel;
-    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Escape))       // key input as "Esc" to exit from Pause Menu
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
-
-    public void Pause()
+    public void Pause()     // Pauses the game when the pause button is triggered
     {
         PausePanel.SetActive(true);
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 
-    public void Continue()
+    public void Resume()    // To resume the game from pause
     {
         PausePanel.SetActive(false);
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMainMenu()  // Load to Main Menu
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Edris' Scene");
     }
 }
+
+    // IF the pause menu requires a quit button function to quit the entire game directly.
+    /* public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
+    */
