224230454c02090aa1b551c06ea92a098ce8200a
Nhanmoi <e.foo@digipen.edu>
2025-09-23 20:49:26 +0800

    Function the Music&SFX Slider
    Added Audio Mixer and Test-Play the input of music (placeholder). Fixed some audio issue in some scenes. Music is implemented but the SFX should be functioning, but will refined it again if its not functioning well...

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 //using UnityEditor.SearchService;
 using UnityEngine;
@@ -8,6 +10,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    private void Start()
+    {
+        LoadVolume();
+        SoundManager.Instance.PlayMusic("Edris' Scene");
+    }
     public void PlayGame()
     {
 
@@ -22,4 +33,31 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();     //To leave the game
     } 
+
+    // This Section below is all for the Music and Sound Setting volume functions.
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume); // in the settings, to adjust the volume of the MusicVolume
+    }
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);   // in the settings, to adjust the volume of the SFXVolume
+    }
+
+    // to save the volume setting
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    // To Load the Volume setting previously to the sliders
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
 }
